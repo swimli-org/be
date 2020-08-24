bcbd802ccac4f0e68ad67354a271200126ec40a7
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-08-23 23:39:18 -0500

    init

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: { 
+      filename: './database/swimli.db3' 
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: { 
+      directory: './database/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  }
+
+};
